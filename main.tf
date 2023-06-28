044714d29dc34bef46b57a9b6bfc6c99b4a41c8c
esavvid <e.savvidou@aurubis.com>
2023-06-28 13:48:56 +0200

    terraform version

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.61.0"
+    }
+  }
+}
+
+
 provider "azurerm" {
   version = "<=3.61.0"
 
