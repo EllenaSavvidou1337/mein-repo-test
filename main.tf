cd90d9f43e09495d3245dbc62c1fb36dd512c251
esavvid <e.savvidou@aurubis.com>
2023-06-28 14:02:03 +0200

    terraform version

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
 terraform {
   required_version = ">= 1.5.1"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.61.0"
     }
   }
-}  
-subscription_id = var.subscription_id
-client_id = var.clientID
-client_secret = var.clientSecret
-tenant_id = var.tenantID
+}
 
-
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.clientID
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantID
+}
 
 resource "azurerm_resource_group" "rg-test" {
-    name = var.resource_group_name
-    location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
