7ace9118872ab77595a1c349ded60d704f6bcce6
esavvid <e.savvidou@aurubis.com>
2023-06-28 13:56:56 +0200

    terraform version

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ terraform {
       version = "3.61.0"
     }
   }
-  
+}  
 subscription_id = var.subscription_id
 client_id = var.clientID
 client_secret = var.clientSecret
 tenant_id = var.tenantID
 
-}
+
 
 resource "azurerm_resource_group" "rg-test" {
     name = var.resource_group_name
