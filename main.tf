2d3835e00bb8b91a4119be05dea2688088a081ad
esavvid <e.savvidou@aurubis.com>
2023-06-29 13:39:52 +0200

    terraform version

--- main.tf
+++ main.tf
@@ -16,3 +16,33 @@ resource "azurerm_resource_group" "rg-test" {
   name     = var.resource_group_name
   location = var.location
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "clientID" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "clientSecret" {
+  type        = string
+  description = "Azure Client Secret"
+}
+
+variable "tenantID" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the Azure Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "Location for the Azure Resource Group"
+}
